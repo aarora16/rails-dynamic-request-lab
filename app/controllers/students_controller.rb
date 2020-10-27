0072a1b8fe01e95db7ccc892772e702d0bf0a107
aarora16 <a.99.anshul@gmail.com>
2020-10-27 14:13:50 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    find_student = Student.find(params[:id])
+    @student = find_student.first_name + " " + find_student.last_name
+  end
 end
